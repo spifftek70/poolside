ffcb56555e9552704b4bf52398528d8a42cf7ef1
dean <spifftek@me.com>
2024-07-07 15:27:19 -0700

    &&

--- public/javascripts/mainjs.js
+++ public/javascripts/mainjs.js
@@ -115,8 +115,8 @@ $(function () {
           $("#poolCirculation").addClass("btn-circ");
           $("#poOn").text(" Pool On");
           $(".gauge").show();
-          $("#pumpRPM").text(ee + " RPM | ");
-          $("#pumpGPM").text(gg + " GPM | ");
+          $("#pumpRPM").text(ee + " RPM | &nbsp;");
+          $("#pumpGPM").text(gg + " GPM | &nbsp;");
           $("#pumpWatt").text(ff + " Watt");
         } else {
           $("#poolCirculation").removeClass("btn-circ");
