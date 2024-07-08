95eb6b7a92e986f20b502a379e5c8b5b7f0732fb
dean <spifftek@me.com>
2024-07-08 07:52:20 -0700

    plus

--- public/javascripts/mainjs.js
+++ public/javascripts/mainjs.js
@@ -209,12 +209,12 @@ $(function () {
         if (field.name === "Spa") {
           aa = field.id;
           bb = field.isOn;
-          $("#spaCurrentTemps").text("Is now: ", field.temp, "° F");
+          $("#spaCurrentTemps").text("Is now: " + field.temp + "° F");
           const spaCoolSetpt = field.coolSetpoint;
           const spaSetPt = field.setPoint;
           const spaVals = spaSetPt.toString() + ", " + spaCoolSetpt.toString();
           $("#spaSlider").roundSlider("setValue", spaVals);
-          $("#spaSetTemps").text("Set for: ", spaVals, "° F");
+          $("#spaSetTemps").text("Set for: " + spaVals + "° F");
           if ('heatStatus' in field) {
             cc = field.heatStatus;
             dd = cc.desc;
