371bc4df52b135e81aaf9699b559ee6e244b2a58
Dean <spifftek@me.com>
2025-05-09 10:09:37 -0700

    nginx try

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { Server } = require('socket.io');
 const createError = require('http-errors');
+const basicAuth = require('express-basic-auth');
+require('dotenv').config(); // For environment variables
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -13,10 +15,26 @@ const io = new Server(httpServer, {
   transports: ['websocket'],
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Basic authentication for Express routes
+app.use(basicAuth({
+  users: { 'admin': process.env.AUTH_PASSWORD || 'yourpassword' }, // Use env variable
+  challenge: true,
+  realm: 'PoolController',
+}));
 
-// view engine setup
+// Socket.IO authentication (optional, uncomment to enable)
+io.use((socket, next) => {
+  const auth = socket.handshake.auth;
+  if (auth && auth.username === 'admin' && auth.password === process.env.AUTH_PASSWORD) {
+    return next();
+  }
+  return next(new Error('Authentication error'));
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -29,28 +47,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Static file serving
 app.use('/stylesheets', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/javascripts', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/javascripts', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/javascripts', express.static(path.join(__dirname, 'public/javascripts/appCalls')));
+app.use('/js/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/js/app', express.static(path.join(__dirname, 'public/javascripts/appCalls')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-// Start the HTTP server on port from environment variable or default to 3000
+// Start the HTTP server
 const port = process.env.PORT || 3000;
 httpServer.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
@@ -62,7 +78,6 @@ io.on('connection', (socket) => {
 
   socket.on('message', (message) => {
     console.log(`Received message => ${message}`);
-    // Broadcast the received message to all clients
     socket.broadcast.send(message);
   });
 
